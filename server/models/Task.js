18c886972d6e4ed64797405e1d919967b893836d
natchaowo <natchaez@gmail.com>
2026-03-12 04:39:16 +0700

    feat: smart task board

---
+++ server/models/Task.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  priority: {
+    type: String,
+    enum: ["High", "Medium", "Low"],
+    default: "Medium"
+  },
+  status: {
+    type: String,
+    enum: ["Pending", "Completed"],
+    default: "Pending"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Task", taskSchema);
